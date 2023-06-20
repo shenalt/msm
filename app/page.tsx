da2529c342bb413df97dcac5eb5ed5b779454496
Shenal Tissera <tisserashenal@gmail.com>
2023-06-20 18:45:07 -0400

    changed cldimage src

--- app/page.tsx
+++ app/page.tsx
@@ -69,56 +69,56 @@ export default function Home() {
 
       <div className="carousel w-full m-4">
       <div id="slide1" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/honeyHeistCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide8" className="btn btn-circle">❮</a> 
           <a href="#slide2" className="btn btn-circle">❯</a>
         </div>
       </div> 
       <div id="slide2" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/TenCandlesCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">❮</a> 
           <a href="#slide3" className="btn btn-circle">❯</a>
         </div>
       </div> 
       <div id="slide3" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/WanderhomeCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">❮</a> 
           <a href="#slide4" className="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/goodSocietyCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle">❮</a> 
           <a href="#slide5" className="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide5" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/mfdBasicCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">❮</a> 
           <a href="#slide6" className="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide6" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/PlaytestCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide5" className="btn btn-circle">❮</a> 
           <a href="#slide7" className="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide7" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/ChickenCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide6" className="btn btn-circle">❮</a> 
           <a href="#slide8" className="btn btn-circle">❯</a>
         </div>
       </div>
       <div id="slide8" className="carousel-item relative w-full">
-        <CldImage width="375" height="375" src={`samples/msm/DieCoverImage`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
+        <CldImage width="375" height="375" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687291351/samples/msm/goodSocietyCoverImage.png`} alt="test" className="w-7/12 lg:w-5/12 h-auto m-auto" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-1 md:left-5 right-1 md:right-5 top-1/2">
           <a href="#slide7" className="btn btn-circle">❮</a> 
           <a href="#slide1" className="btn btn-circle">❯</a>
