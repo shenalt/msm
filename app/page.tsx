29d5e53c8d2e96a6afcd8188e410312301ab641f
Shenal Tissera <tisserashenal@gmail.com>
2023-07-09 21:27:45 -0400

    edits that shouldve happened a while ago

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,25 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-3 overflow-x-hidden">
       {/* DESKTOP */}
-      <div className='hidden lg:flex flex-row w-full lg:w-2/3 mx-4 my-4 border-t border-b border-t-msmRed border-b-msmBlue justify-evenly items-center'>
+      <div className='hidden lg:flex flex-col w-full lg:w-2/3 mx-4 mt-4 mb-1 border-t border-b border-t-msmRed border-b-msmBlue justify-evenly items-center'>
         <div className='w-1/2'>
           <p className='my-4 text-lg'>
-            Hello, and welcome to Many Sided Media! You may know us best as the team behind My First Dungeon, the Twenty Sided Podcast, and the <Link className="text-newsletterTint hover:text-newsletterPurple transition-all duration-500" target="_blank" href={'https://twentysidednewsletter.substack.com/'}>Twenty Sided Newsletter</Link> 
-            . But with a love of games as big as ours, you know we couldn’t just stop there. We’ve got big plans for the future, 
+            Hello, and welcome to Many Sided Media! You may know us best as the team behind My First Dungeon, the Twenty Sided Podcast, and the Twenty Sided Newsletter. 
+            But with a love of games as big as ours, you know we couldn’t just stop there. We’ve got big plans for the future, 
             with the goal of bringing you even more TTRPG content, and this is the absolute best place to keep up to date with each exciting new venture. 
           </p>
         </div>
-        <div className='flex flex-col justify-center items-center'>
-          <Link className='w-3/5' href={'https://www.patreon.com/MyFirstDungeonPod/creators'} target='_blank'><button className='p-2 m-2 font-bold bg-patreonOrange text-whiteColor hover:animate-pulseFP'>Join our Patreon</button></Link>
-          <Link className='w-3/5' href={'https://twentysidednewsletter.substack.com/'} target='_blank'><button className='p-2 m-2 font-bold bg-newsletterPurple text-whiteColor hover:animate-pulseFP'>Subscibe to the Newsletter</button></Link>
+        <div className='flex flex-row justify-center items-center my-4'>
+          <Link className='w-36' href={'https://www.patreon.com/MyFirstDungeonPod/creators'} target='_blank'><button className='p-2 m-2 font-bold bg-patreonOrange text-whiteColor hover:animate-pulseFP'>Join our Patreon           </button></Link>
+          <Link className='w-36' href={'https://twentysidednewsletter.substack.com/'} target='_blank'><button className='p-2 m-2 font-bold bg-newsletterPurple text-whiteColor hover:animate-pulseFP'>Subscribe to the Newsletter</button></Link>
         </div>
       </div>
       {/* DESKTOP */}
       {/* MOBILE & TABLET */}
-      <div className='lg:hidden flex flex-row mt-4 md:gap-16'>
+      {/* <div className='lg:hidden flex flex-row mt-4 md:gap-16'>
         <Link className='w-3/5' href={'https://www.patreon.com/MyFirstDungeonPod/creators'} target='_blank'><button className='h-4/5 p-4 m-2 font-bold bg-patreonOrange text-whiteColor hover:animate-pulseFP'>Join our Patreon</button></Link>
         <Link className='w-3/5' href={'https://twentysidednewsletter.substack.com/'} target='_blank'><button className='h-4/5 p-4 m-2 font-bold bg-newsletterPurple text-whiteColor hover:animate-pulseFP'>Subscibe to the Newsletter</button></Link>
-      </div>
+      </div> */}
       {/* MOBILE & TABLET */}
       <Image 
         src={frontPageLogo} 
@@ -36,12 +36,16 @@ export default function Home() {
       />
       <div className='w-full lg:w-2/3 m-4 border translate lg:border-l-0 lg:border-r-0 border-t-msmRed border-b-msmBlue border-l-msmYellow border-r-msmYellow'>
         <p className='lg:hidden m-8 text-lg'>
-          Hello, and welcome to Many Sided Media! You may know us best as the team behind My First Dungeon, the Twenty Sided Podcast, and the <Link className="text-newsletterTint hover:text-newsletterPurple transition-all duration-500" target="_blank" href={'https://twentysidednewsletter.substack.com/'}>Twenty Sided Newsletter</Link> 
+          Hello, and welcome to Many Sided Media! You may know us best as the team behind My First Dungeon, the Twenty Sided Podcast, and the Twenty Sided Newsletter
           . But with a love of games as big as ours, you know we couldn’t just stop there. We’ve got big plans for the future, 
           with the goal of bringing you even more TTRPG content, and this is the absolute best place to keep up to date with each exciting new venture. 
         </p>
+        <div className='flex flex-row justify-center items-center my-4 lg:hidden'>
+          <Link className='w-36' href={'https://www.patreon.com/MyFirstDungeonPod/creators'} target='_blank'><button className='p-2 m-2 font-bold bg-patreonOrange text-whiteColor hover:animate-pulseFP'>Join our Patreon           </button></Link>
+          <Link className='w-36' href={'https://twentysidednewsletter.substack.com/'} target='_blank'><button className='p-2 m-2 font-bold bg-newsletterPurple text-whiteColor hover:animate-pulseFP'>Subscribe to the Newsletter</button></Link>
+        </div>
         <p className='m-8 text-lg'>
-          P.S. Our <Link href={"https://www.patreon.com/MyFirstDungeonPod/creators"} target='_blank' className="text-patreonTint hover:text-patreonOrange transition-all duration-500">My First Dungeon Patreon, The Dice Pool</Link>, is now live!
+          P.S. Our My First Dungeon Patreon, The Dice Pool, is now live!
         </p>
         <p className='m-8 text-lg'>
           Patrons get access to even more actual play episodes featuring both new games and old favorites, as well as cast talk backs for each season, interviews with TTRP all-stars and more fun goodies. There’s 
@@ -50,8 +54,8 @@ export default function Home() {
         </p>
       </div>
 
-      <Link href={'/myfirstdungeon'}>
-        <div className='flex flex-row flex-wrap items-center justify-center my-8'>       
+      <Link href={'/myfirstdungeon'} className='hidden md:flex'>
+        <div className='hidden md:flex flex-row flex-wrap items-center justify-center my-8'>       
             <CldImage width="200" height="200" src={'https://res.cloudinary.com/djr5bxwfk/image/upload/v1686269652/samples/msm/TenCandlesCoverImage.jpg'} alt='TenCandles' />
             <CldImage width="200" height="200" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687290373/samples/msm/ChickenCoverImage.png`} alt="Something is Wrong with the Chickens" />
             <CldImage width="200" height="200" src={`https://res.cloudinary.com/djr5bxwfk/image/upload/v1687288452/samples/msm/PlaytestCoverImage.jpg`} alt="Schroedinger's Cat" />
@@ -62,7 +66,7 @@ export default function Home() {
         </div>
       </Link>
 
-      {/* <div className="carousel w-full m-4">
+      <div className="md:hidden carousel w-full m-4">
       <div id="slide1" className="carousel-item relative w-full">
         <Link className="w-7/12 lg:w-5/12 h-auto m-auto" href={'/myfirstdungeon'}>
           <CldImage width="1024" height="1024" src={'https://res.cloudinary.com/djr5bxwfk/image/upload/v1686269652/samples/msm/TenCandlesCoverImage.jpg'} alt='TenCandles' />
@@ -126,7 +130,7 @@ export default function Home() {
           <a href="#slide1" className="btn btn-circle">❯</a>
         </div>
       </div>
-    </div> */}
+    </div>
 
     </main>
   )
